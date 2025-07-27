c6cb2e5368187e5e5f1cd64e3dd83c2bb849e402
Greg <harper357@gmail.com>
2025-07-27 10:43:02 -0700

    added empty class

---
+++ multiqc/modules/gunc/gunc.py
@@ -0,0 +1,11 @@
+import logging
+
+from multiqc.base_module import BaseMultiqcModule, ModuleNoSamplesFound
+from multiqc.plots import table
+from multiqc.plots.table_object import TableConfig
+
+log = logging.getLogger(__name__)
+
+
+class MultiqcModule(BaseMultiqcModule):
+    pass
